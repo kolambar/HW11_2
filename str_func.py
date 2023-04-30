a28502ed1569b92bc9cb430103afb47647ab05b8
Nikolai <kolamba.r@gmail.com>
2023-04-30 13:01:59 +0300

    add to str_func function which swiches first letters in words to capitals. also add docstrings

--- str_func.py
+++ str_func.py
@@ -1,2 +1,8 @@
 def swich_to_capitals(word):
+    """делает буквы в слове большими"""
     return word.upper()
+
+
+def do_first_let_big(string):
+    """делает большими все первые буквы"""
+    return string.title()
